c138b92558760b6f9d1016b316a3df526845de44
bikram455 <bikramleeemboo@gmail.com>
2021-06-07 22:36:55 +0545

    * Changes in package.json.

--- routes.js
+++ routes.js
@@ -1,14 +1,39 @@
 import Crawler from 'crawler';
+import twilio from 'twilio';
+import Cheerio from 'cheerio';
 import { Router } from 'express';
 
 const routes = Router();
+const client = twilio('AC22d74ad29d606dcb5789bc950a5bb1d2', '05ccbb5c299ea89f816f1b6a79b7cb14');
+// const client = require('twilio')('AC22d74ad29d606dcb5789bc950a5bb1d2', '05ccbb5c299ea89f816f1b6a79b7cb14');
+const cheerio = Cheerio;
 
 routes.get('/', (req, res) => {
-    testCrawler()
+    // testCrawler()
+    sendMessage();
     res.send({
         message: 'This is a test web crawler route.'
     });
 });
+
+
+const sendMessage = () => {
+    console.log('Inside message sender...');
+    client.messages
+    // Sending a SMS using message server
+    // .create({
+    //     body: 'This is a trial message sent from twilio npm.',
+    //     from: '+18187148469',
+    //     to: '+9779807921240'
+    // })
+    .create({ 
+        body: 'This is a trial message sent from twilio npm.',
+        from: 'whatsapp:+14155238886',       
+        to: 'whatsapp:+9779807921240' 
+      }) 
+    .then(message => console.log('Message sent successfully.'))
+    .catch(err => console.log('Error while sending message: ', err)).done();
+}
 
 const testCrawler = () => {
     console.log('inside crawler..')
@@ -20,6 +45,11 @@ const testCrawler = () => {
             }else{
                 var $ = res.$;
                 console.log($("title").text());
+                const html = cheerio.load(res.body);
+                const links = html('a');
+                links.each((index, element) => {
+                    console.log(`https://hamrobazaar.com/${html(element).attr('href')}`)
+                });
             }
             done();
         }
